2e173647a0861e277f02b4d51d02ac1854e9da0f
SinaSystem <sinasystem47@gmail.com>
2020-06-04 21:50:00 +0430

    Initial commit

---
+++ app/src/main/java/com/sriyanksiddhartha/multiplescreendemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sriyanksiddhartha.multiplescreendemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ * 	Author: Sriyank Siddhartha
+ *
+ * 	Module7: Using Qualifiers with Values Directories to Design Layouts
+ *
+ * 			"AFTER" project
+ * */
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+}
